fae12f83312a4ff7d2426c1d9ea1999a1a4d0d70
amarduwal <duwal.amar2006@gmail.com>
2025-02-08 15:30:42 +0545

    Check active event loop

--- bot.py
+++ bot.py
@@ -213,14 +213,12 @@ def webhook():
     update_data = request.get_json(force=True)
     update = Update.de_json(update_data, application.bot)
 
-    # Process the update asynchronously
-    asyncio.create_task(application.process_update(update))
-    # # Check if there's an active event loop
-    # try:
-    #   loop = asyncio.get_running_loop()
-    #   loop.create_task(application.process_update(update))  # Run in existing loop
-    # except RuntimeError:
-    #   asyncio.run(application.process_update(update))  # Run in a new loop
+    # Check if there's an active event loop
+    try:
+      loop = asyncio.get_running_loop()
+      loop.create_task(application.process_update(update))  # Run in existing loop
+    except RuntimeError:
+      asyncio.run(application.process_update(update))  # Run in a new loop
 
     return Response("OK", status=200)
   except Exception as e:
